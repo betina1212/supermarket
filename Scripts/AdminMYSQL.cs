68a777784047763fdd814cf25e899cf1dde73486
DarioGP150898 <vacafeliz123.dg@gmail.com>
2019-11-29 23:41:51 -0400

    Primera bitacora

---
+++ Scripts/AdminMYSQL.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using MySql.Data.MySqlClient;
+
+public class AdminMYSQL : MonoBehaviour
+{
+
+    public string servidorBaseDatos;
+    public string nombreBaseDatos;
+    public string usuarioBaseDatos;
+    public string contraseñaBaseDatos;
+
+
+    private string datosConexion;
+    private MySqlConnection conexion;
+
+    void Start()
+    {
+
+        datosConexion = "Server=" + servidorBaseDatos
+                      + ";DataBase=" + nombreBaseDatos
+                      + ";Uid=" + usuarioBaseDatos
+                      + ";Pwd=" + contraseñaBaseDatos
+                      + ";";
+
+        ConectarConServidorBaseDatos();
+
+    }
+
+    private void ConectarConServidorBaseDatos()
+    {
+        conexion = new MySqlConnection(datosConexion);
+
+        try
+        {
+            conexion.Open();
+            Debug.Log("Conexion con BD correcta!");
+        }
+
+        catch(MySqlException error)
+        {
+            Debug.LogError("Imposible conectar con Base de Datos" + error);
+        }
+    }
+
+}
