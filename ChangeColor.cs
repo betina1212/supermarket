c42156906137f965364dcef85ea3175f8c596ff5
betina1212 <56407228+betina1212@users.noreply.github.com>
2019-12-10 21:13:32 -0400

    Scripts

---
+++ ChangeColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+    public void Green()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+    public void Black()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+}
---
+++ ProductController.cs
@@ -0,0 +1,63 @@
+﻿namespace GoogleVR.HelloVR
+{
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+  [RequireComponent(typeof(Collider))]
+public class ProductController : MonoBehaviour
+{
+    private Vector3 startingPosition;
+        private Renderer myRenderer;
+        public static int stock=50;
+        public int item;
+     public void Reset()
+        {
+            int sibIdx = transform.GetSiblingIndex();
+            int numSibs = transform.parent.childCount;
+            for (int i = 0; i < numSibs; i++)
+            {
+                GameObject sib = transform.parent.GetChild(i).gameObject;
+                sib.transform.localPosition = startingPosition;
+                sib.SetActive(i == sibIdx);
+            }
+        }
+
+        /// <summary>Calls the Recenter event.</summary>
+        public void Recenter()
+        {
+#if !UNITY_EDITOR
+            GvrCardboardHelpers.Recenter();
+#else
+            if (GvrEditorEmulator.Instance != null)
+            {
+                GvrEditorEmulator.Instance.Recenter();
+            }
+#endif  // !UNITY_EDITOR
+        }
+
+        /// <summary>Teleport this instance randomly when triggered by a pointer click.</summary>
+        /// <param name="eventData">The pointer click event which triggered this call.</param>
+        public void TeleportRandomly(BaseEventData eventData)
+        {
+            // Only trigger on left input button, which maps to
+            // Daydream controller TouchPadButton and Trigger buttons.
+            PointerEventData ped = eventData as PointerEventData;
+            if (ped != null)
+            {
+                if (ped.button != PointerEventData.InputButton.Left)
+                {
+                    return;
+                }
+            }
+
+            ProductsList.products[item].stock--;
+        }
+
+        private void Start()
+        {
+            startingPosition = transform.localPosition;
+            myRenderer = GetComponent<Renderer>();
+        }
+}
+}
