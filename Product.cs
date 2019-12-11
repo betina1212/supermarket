df64aca134eb98c3ed8537775c17fedffc949597
DESKTOP-7QUBBDD\RODRIGO <rodridelto@gmail.com>
2019-12-11 09:37:02 -0400

    Sprint 2

---
+++ Product.cs
@@ -0,0 +1,30 @@
+﻿namespace GoogleVR.HelloVR
+{
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Product : MonoBehaviour
+{
+    public string nombre;
+    public int stock;
+    public float precio;
+
+    public Product(string nombre, int stock, float precio) {
+        this.nombre = nombre;
+        this.stock = stock;
+        this.precio = precio;
+//     Start is called before the first frame update
+    void Start()
+    {
+    }
+
+  //   Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
+}
