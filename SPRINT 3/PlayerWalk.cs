dc20daf584e8de4f908843bd3d9b256bac43bc47
betina1212 <56407228+betina1212@users.noreply.github.com>
2019-12-20 22:06:09 -0400

    scripts

---
+++ SPRINT 3/PlayerWalk.cs
@@ -0,0 +1,57 @@
+﻿namespace GoogleVR.HelloVR
+{
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerWalk : MonoBehaviour {
+
+
+    public TextMesh totalCoins;
+    public float playerSpeed;
+    public GameObject wall;
+    public int count;
+    private string selectedProducts="";
+    public TextMesh productsText;
+    private int totalproducts=0;
+
+    void start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+        totalCoins.text = "Productos \n Elegidos";
+        if(ProductsList.productsSelected.Count != totalproducts)
+        {
+            selectedProducts = "";
+            for (int i = 0; i < ProductsList.productsSelected.Count; i++)
+            {
+                selectedProducts+=ProductsList.productsSelected[i].Nombre_Producto+"\n";
+                productsText.text = selectedProducts;
+            }
+            totalproducts = ProductsList.productsSelected.Count;
+        }
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            count++;
+            totalCoins.text = count.ToString();
+            if (count == 4)
+            {
+                wall.SetActive(false);
+            }
+        }
+    }*/
+}
+}
---
+++ SPRINT 3/WebServices.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Net;
+using UnityEngine.SceneManagement;
+
+public class WebServices : MonoBehaviour
+{
+    public Text texto;
+    public Text user;
+    public Text pass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void VerificarUsuario()
+	{
+		userModel a;
+		try{
+            Debug.Log(user.text);
+		var json = new WebClient().DownloadString(string.Format("http://192.168.42.232/Gestion/api/Usuarios/{0}", user.text));
+		a = JsonUtility.FromJson<userModel>(json);//JsonConvert.DeserializeObject<bool> (json);
+			if(a._Usuario == null){
+                Debug.Log("datos incorrectos");
+                texto.text = "datos incorrectos";
+                //SceneManager.LoadScene("lvl_01");
+                //SceneCtrl.ChangeScene("lvl_01");
+            }
+            else{
+                SceneManager.LoadScene("lvl_01");
+                //SceneCtrl.ChangeScene("lvl_01");
+            }
+		}catch(Exception){
+            Debug.Log("fallo en la conexion");
+			texto.text = "fallo en la conexion";
+            //SceneManager.LoadScene("lvl_01");
+		}
+	}
+}
+
+public class userModel{
+    private string Usuario;
+    private string Password;
+
+    public userModel(){
+        Usuario = "";
+        Password = "";
+    }
+
+    public string _Usuario { get{ return Usuario;} set{Usuario = value; } }
+	public string _Password { get{ return Password;} set{Password = value; } }
+}
